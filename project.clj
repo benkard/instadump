5d1bb777258505b2edd4330613413e2f1c9227de
Matthias Andreas Benkard <code@mail.matthias.benkard.de>
2012-02-21 23:24:41 +0100

    Bump version to 0.0.2.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject instadump "0.0.1"
+(defproject instadump "0.0.2"
   :description "Semi-automatic serialization for Clojure vars"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [com.sleepycat/je "4.1.10"]  ;4.0.96 if the Oracle Maven repo scares you
